2d1fc6ea3d039f4bdf14da0de352f26af7733157
0x1d107 <yablinchik@gmail.com>
2023-05-18 17:57:15 +0500

    report

--- gensens.sh
+++ gensens.sh
@@ -2,7 +2,7 @@
 rm -rf build
 meson setup build -Dbuildtype=release
 # 8 16 32 64 100 128 150 200 250 300
-for RR in $(seq 0 0.01 0.12)
+for RR in 0.016 $(seq 0.035 0.002 0.045)
 do
 meson configure build -Dcpp_args=-DRR=$RR
 bash plot.sh detector
